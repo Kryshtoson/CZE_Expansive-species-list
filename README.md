6577b9847c2d1d21673117c897494536d862e3a1
Kryshtoson <krystof.chytry@gmail.com>
2023-07-03 11:19:40 +0200

    strategies

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # The list of expansive plants of the Czech Republic
 
+* I keep archeophytes in.
+
 ## Scripts
 
 * `Proportion_within_families.R`: creates `Families.png` 
@@ -8,4 +10,12 @@
 
 * `Lifeforms_growthforms.R`: creates `Lifeforms.png`
   * Proportion of individual lifeforms of expansive species vs all species in Czech flora.
+  * All families included.
+
+* `New_maps.R`: creates `Expansive_plants.pdf`
+  * For Appendix, I suppose
+  * All species included
+
+* `Strategies.R`: creates `Strategies.png`
+  * Boxplots of Pierce's indices
   * All families included.
--- scripts/Strategies.R
+++ scripts/Strategies.R
@@ -2,13 +2,13 @@ library(readxl)
 library(tidyverse)
 
 traity <- read_xlsx(r'(C:/Users/krystof/OneDrive - MUNI/2022_Expanzky/Expanzky_traity_2023-05-09.xlsx)')
-view(traity)
+
 bind_rows(traity,
           traity |>
             filter(expansive == 'expansive') |>
             mutate(expansive = 'no')) |>
   mutate(expansive = factor(expansive, levels = c('no', 'expansive'),
-                labels = c('Czech flora', 'Expansive species'))) |>
+                            labels = c('Czech flora', 'Expansive species'))) |>
   select(species, expansive, `C-PIERCE`:`R-PIERCE`) |>
   pivot_longer(`C-PIERCE`:`R-PIERCE`) |>
   mutate(name = gsub('-PIERCE', '', name),
@@ -16,8 +16,12 @@ bind_rows(traity,
   ggplot(aes(expansive, value)) +
   geom_violin(aes(fill = expansive), alpha = 1, show.legend = F) +
   #geom_boxplot(aes(fill = expansive), ) +
-  geom_boxplot(width=0.1, alpha = .2) +
+  geom_boxplot(width = 0.1, alpha = .2) +
   scale_fill_manual(values = c('grey88', '#FFC300')) +
   facet_grid(~name, scales = 'free') +
-  theme_bw()
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(size = 13, face = 'bold'),
+        axis.title = element_blank())
 
+ggsave('/figures/Strategies.png', height = 4, width = 7)
